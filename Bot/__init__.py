69d34f71de954d6fba0bd8659596b179b4585e7c
Madara Uchiha <141416632+Vikash106@users.noreply.github.com>
2024-01-03 23:36:22 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -6,7 +6,7 @@ if os.path.exists('error.log'):
         
 #<-----------LOGGING------------>
 logging.basicConfig(level=logging.INFO, filename='error.log')
-LOG = logging.getLogger("Bot by @soheru")
+LOG = logging.getLogger("Bot by @ItzMonkeyDLuffy")
 LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
@@ -17,12 +17,12 @@ APP_HASH = os.environ.get('APP_HASH', 'your api id')# Telgram App hash
 OWNER_ID = int(os.environ.get('OWNER_ID', 953362604))
 MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
 FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -100456789013)
-BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
+BOT_NAME = os.environ.get('BOT_NAME', 'ItzMonkeyDLuffy')
 #<---------------Connecting-------------->
 if BOT_TOKEN is not None:
     try:
         encoder  = Client('AutoEncoder', api_id=API_ID, api_hash=APP_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
-        LOG.info('❤️ Bot Connected Created By Github @soheru || Telegram @sohailkhan_indianime ')
+        LOG.info('❤️ Bot Connected Created By Github @ItzMonkeyDLuffy || Telegram @ItzMonkeyDLuffy ')
     except Exception as e:
         LOG.warn(f'😞 Error While Connecting To Bot\nCheck Errors: {e}')
         sys.exit()       
