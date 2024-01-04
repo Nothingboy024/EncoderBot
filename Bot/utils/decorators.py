204d6083712c18066a292ec259b0b78d6cb91619
Madara Uchiha <141416632+Vikash106@users.noreply.github.com>
2024-01-04 08:22:17 +0530

    Update decorators.py

--- Bot/utils/decorators.py
+++ Bot/utils/decorators.py
@@ -39,9 +39,9 @@ def ffmpeg_settings(id, input, FT):
         if resolutio == '480p':
             cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -s {resolution} -metadata title="Telegram - @Animestation2" -metadata:s:v title="@Animestation2 - {resolutio} - {vcodec}"  -metadata:s:a title="@Animestation2" -map 0:v -c:a aac -b:a 64k -map 0:a -c:s copy -map 0:s? """{output}""" -y'
         elif resolutio == '720p':
-            cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -s {resolution} -metadata title="Telegram - @Animestation2e" -metadata:s:v title="@Animestation2 - {resolutio} - {vcodec}"  -metadata:s:a title="@Animestation2" -map 0:v -c:a aac -b:a 128k -map 0:a -c:s copy -map 0:s? """{output}""" -y'
+            cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -s {resolution} -metadata title="Telegram - @Animestation2" -metadata:s:v title="@Animestation2 - {resolutio} - {vcodec}"  -metadata:s:a title="@Animestation2" -map 0:v -c:a aac -b:a 128k -map 0:a -c:s copy -map 0:s? """{output}""" -y'
         else:
-            cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -metadata title="Encoded By IndiAnime" -metadata:s:v title="IndiAnime - {resolutio} - {vcodec}"  -metadata:s:a title="IndiAnime" -map 0:v -c:a aac -b:a 256k -map 0:a -c:s copy -map 0:s? """{output}""" -y'
+            cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -metadata title="Telegram - @Animestation2" -metadata:s:v title="Animestation2 - {resolutio} - {vcodec}"  -metadata:s:a title="Animestation2" -map 0:v -c:a aac -b:a 256k -map 0:a -c:s copy -map 0:s? """{output}""" -y'
     
     elif audio_type == 'libopus':
         if resolutio == '480p':
@@ -49,7 +49,7 @@ def ffmpeg_settings(id, input, FT):
         elif resolutio == '720p':
             cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -s {resolution} -metadata title="Telegram - @Animestation2" -metadata:s:v title="@Animestation2 - {resolutio} - {vcodec}"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 128k -map 0:a -c:s copy -map 0:s? """{output}""" -y'
         else:
-            cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -metadata title="Encoded By IndiAnime" -metadata:s:v title="IndiAnime - {resolutio} - {vcodec}"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 256k -map 0:a -c:s copy -map 0:s? """{output}""" -y'        
+            cmd = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec {vcodecs} -crf {crf} -metadata title="Telegram - @Animestation2" -metadata:s:v title="Animestation2 - {resolutio} - {vcodec}"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 256k -map 0:a -c:s copy -map 0:s? """{output}""" -y'        
     return cmd      
 
 # def ffmpeg_auto_settings(id, input, FT):
@@ -94,9 +94,9 @@ def ffmpeg_settings(id, input, FT):
 #     elif audio_type == 'libopus':
 #         if resolutio == 'auto':
 #             if vcodecs == 'libx265':
-#                 lq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx265 -crf 26 -s 640x480 -metadata title="Encoded By IndiAnime" -metadata:s:v title="IndiAnime - 480p - libx265"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 64k -map 0:a -c:s copy -map 0:s? """{output}480p.mkv""" -y'
-#                 mq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx265 -crf 23 -s 1280x720 -metadata title="Encoded By IndiAnime" -metadata:s:v title="IndiAnime - 720p - libx265"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 128k -map 0:a -c:s copy -map 0:s? """{output}720p.mkv""" -y'
-#                 hq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx265 -crf 24 -metadata title="Encoded By IndiAnime" -metadata:s:v title="IndiAnime - 1080p - libx265"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 256k -map 0:a -c:s copy -map 0:s? """{output}1080p.mkv""" -y'
+#                 lq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx265 -crf 26 -s 640x480 -metadata title="Telegram - @Animestation2" -metadata:s:v title="IndiAnime - 480p - libx265"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 64k -map 0:a -c:s copy -map 0:s? """{output}480p.mkv""" -y'
+#                 mq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx265 -crf 23 -s 1280x720 -metadata title="Telegram - @Animestation2" -metadata:s:v title="IndiAnime - 720p - libx265"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 128k -map 0:a -c:s copy -map 0:s? """{output}720p.mkv""" -y'
+#                 hq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx265 -crf 24 -metadata title="Encoded By IndiAnime" -metadata:s:v title="@Animestation2 - 1080p - libx265"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 256k -map 0:a -c:s copy -map 0:s? """{output}1080p.mkv""" -y'
 #             elif vcodecs == 'libx264':
 #                 lq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx264 -crf 28 -s 640x480 -metadata title="Telegram - @Animestation2" -metadata:s:v title="@Animestation2 - 480p - libx264"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 64k -map 0:a -c:s copy -map 0:s? """{output}480p.mkv""" -y'
 #                 mq = f'ffmpeg -hide_banner -loglevel quiet -progress progress-{FT}.txt -i """{input}""" -preset {preset} -vcodec libx264 -crf 27 -s 1280x720 -metadata title="Telegram - @Animestation2" -metadata:s:v title="@Animestation2 - 720p - libx264"  -metadata:s:a title="@Animestation2" -map 0:v -c:a libopus -b:a 128k -map 0:a -c:s copy -map 0:s? """{output}720p.mkv""" -y'
